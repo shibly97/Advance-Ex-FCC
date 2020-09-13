41bf5380da46cb170ebce01606d90d118d368d3e
Glitch (tough-just-salute) <none>
2020-09-13 14:31:01 +0000

    🛳🎻 Checkpoint ./server.js:21184821/3028

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 "use strict";
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
-const myDB = require('./connection');
+const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
@@ -11,9 +11,12 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set("view engine", "pug");
+app.set("views", "./views/pug");
+
 app.route("/").get((req, res) => {
   //Change the response to render the Pug template
-  res.render('Load your view here');
+  res.render("index", { title: "Hello", message: "Please-login" });
 });
 
 app.listen(process.env.PORT || 3000, () => {
