f4932b9648fcc0ee38d54130a06093fe8c627ae1
Glitch (tough-just-salute) <none>
2020-09-13 17:25:15 +0000

    🍎💓 Checkpoint ./server.js:21184821/2267 ./sample.env:21184821/26

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const passport = require("passport");
+const session = require("express-session");
 
 const app = express();
 
@@ -10,6 +12,18 @@ fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false }
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set("view engine", "pug");
 app.set("views", "./views/pug");
