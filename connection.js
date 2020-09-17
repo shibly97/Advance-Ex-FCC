a415d4a2b902332f1644769c4ceb4b8ccafda17d
Glitch (tough-just-salute) <none>
2020-09-17 07:22:15 +0000

    🗽🏫 Checkpoint ./connection.js:21184821/81 ./server.js:21184821/1

--- connection.js
+++ connection.js
@@ -6,19 +6,20 @@ async function main(callback) {
     const URI = process.env.MONGO // Declare MONGO_URI in your .env file
     console.log(URI)
     const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  console.log(client)
 
     try {
         // Connect to the MongoDB cluster
-        await client.connect();
+        const connection = await client.connect();
 
         // Make the appropriate DB calls
-        await callback(client);
+        await callback(connection);
 
     } catch (e) {
         // Catch any errors
         console.error(e);
         throw new Error('Unable to Connect to Database')
-    }
-}
+    } 
+} 
 
 module.exports = main;
