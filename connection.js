27558ead362613e475edeba696c1b5541401e759
Glitch (tough-just-salute) <none>
2020-09-17 08:08:26 +0000

    🍩🌔 Checkpoint ./connection.js:21184821/456

--- connection.js
+++ connection.js
@@ -1,21 +1,20 @@
 // Do not change this file
 require('dotenv').config();
-const { MongoClient } = require('mongodb');
+const MongoClient= require('mongodb').MongoClient;
 
 async function main(callback) {
     const URI = process.env.MONGO // Declare MONGO_URI in your .env file
     console.log(URI)
-    const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
-  console.log(client)
+  
 
     try {
         // Connect to the MongoDB cluster
-        const connection = await client.connect();
+         const client = MongoClient.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
         // Make the appropriate DB calls
-        await callback(connection);
-
-    } catch (e) {
+        await callback();
+ 
+    } catch (e) { 
         // Catch any errors
         console.error(e);
         throw new Error('Unable to Connect to Database')
