17bb049355ceae782d1a561441913b9f26acf2d9
Glitch (tough-just-salute) <none>
2020-09-18 05:52:29 +0000

    🛤🌞 Checkpoint ./server.js:21184821/7470

--- server.js
+++ server.js
@@ -100,40 +100,38 @@ myDB(async client => {
     req.logout();
     res.redirect("/");
   });
-  
-  
-  app.route('/register')
-  .post((
-    req, res, next) => {
-    myDataBase.findOne({ username: req.body.username }, function(err, user) {
-      if (err) {
-        next(err);
-      } else if (user) {
-        res.redirect('/');
-      } else {
-        myDataBase.insertOne({
-          username: req.body.username,
-          password: req.body.password
-        },
-          (err, doc) => {
-            if (err) {
-              res.redirect('/');
-            } else {
-              // The inserted document is held within
-              // the ops property of the doc
-              next(null, doc.ops[0]);
-            }
-          }
-        )
-      }
-    })
-  },
-    passport.authenticate('local', { failureRedirect: '/' }),
+
+  app.route("/register").post(
     (req, res, next) => {
-      res.redirect('/profile');
+      myDataBase.findOne({ username: req.body.username }, function(err, user) {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect("/");
+        } else {
+          myDataBase.insertOne(
+            {
+              username: req.body.username,
+              password: req.body.password
+            },
+            (err, doc) => {
+              if (err) {
+                res.redirect("/");
+              } else {
+                // The inserted document is held within
+                // the ops property of the doc
+                next(null, doc.ops[0]);
+              }
+            }
+          );
+        }
+      });
+    },
+    passport.authenticate("local", { failureRedirect: "/" }),
+    (req, res, next) => {
+      res.redirect("/profile");
     }
   );
-
 
   app.use((req, res, next) => {
     res
