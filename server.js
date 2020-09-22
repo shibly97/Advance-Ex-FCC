275a477b4d8de3c890836483886782fb1d1da8e5
Glitch (tough-just-salute) <none>
2020-09-22 10:52:15 +0000

    💶🙍 Checkpoint ./server.js:21184821/6561

--- server.js
+++ server.js
@@ -13,27 +13,14 @@ const auth = require("./auth");
 
 const app = express();
 
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
-const passportSocketIo = require('passport.socketio')
-const cookieParser = require('cookie-parser')
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const passportSocketIo = require("passport.socketio");
+const cookieParser = require("cookie-parser");
 
-const MongoStore = require('connect-mongo')(session)
-const URI = process.env.MONGO
-const store = new MongoStore({url : URI})
-
-io.use(
-  passportSocketIo.authorize({
-    cookieParser : cookieParser,
-    key : 'express.sid',
-    secret : process.env.SECRET,
-    store : store,
-    success : onAuthorizeSuccess,
-    fail : onAuthorizeFail
-})
-)
-
-
+const MongoStore = require("connect-mongo")(session);
+const URI = process.env.MONGO;
+const store = new MongoStore({ url: URI });
 
 fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
@@ -45,7 +32,9 @@ app.use(
     secret: process.env.SECRET,
     resave: true,
     sveUninitialized: true,
-    cookie: { secure: false }
+    cookie: { secure: false },
+    key: "express.sid",
+    store: store
   })
 );
 
@@ -55,24 +44,48 @@ app.use(passport.session());
 app.set("view engine", "pug");
 app.set("views", "./views/pug");
 
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: "express.sid",
+    secret: process.env.SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
+
+function onAuthorizeSuccess(data, accept) {
+  console.log("successful connection to socket.io");
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log("failed connection to socket.io:", message);
+  accept(null, false);
+}
+
 myDB(async client => {
   const myDataBase = await client.db("fccAdvance").collection("passport");
 
   auth(app, myDataBase, passport, ObjectId, LocalStrategy, bcrypt);
   routes(app, myDataBase, passport, bcrypt);
-  
-  let currentUsers = 0
-  
-  io.on('connection', socket =>{
-    currentUsers ++
-    io.emit('user count', currentUsers)
+
+  let currentUsers = 0;
+
+  io.on("connection", socket => {
+    currentUsers++;
+    io.emit("user count", currentUsers);
     
-    socket.on('disconnect', ()=>{
-      currentUsers --
-      io.emit('user count', currentUsers)
-    })
-    
-  })
+    console.log('user ' + socket.request.user.name + ' connected');
+
+    socket.on("disconnect", () => {
+      currentUsers--;
+      io.emit("user count", currentUsers);
+    });
+  });
 
   // Be sure to add this...
 }).catch(e => {
